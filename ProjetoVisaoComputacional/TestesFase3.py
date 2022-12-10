d24067ffd2c6b13c3e09f0abb74161d8fdd66249
OMeninoPrecavido <andrecoccoconde20@gmail.com>
2022-12-10 21:44:48 +0000

    Base da Fase 3

--- ProjetoVisaoComputacional/TestesFase3.py
+++ ProjetoVisaoComputacional/TestesFase3.py
@@ -4,7 +4,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-# eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 
 while True:
     ret, frame = cap.read()
@@ -13,11 +13,13 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5) #retorna a posição de todos os rostos
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
-        # roi_gray = gray[y:y+w, x:x+w]
-        # roi_color = frame[y:y+h, x:x+w]
-        # eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
-        # for (ex, ey, ew, eh) in eyes:
-        #     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+        roi_gray = gray[y:y+w, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+
+
 
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) == ord('q'):
