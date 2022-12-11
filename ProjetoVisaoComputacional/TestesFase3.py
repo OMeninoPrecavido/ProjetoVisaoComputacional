ac1d0fa1014550494facda1312bb8901a11754a5
OMeninoPrecavido <andrecoccoconde20@gmail.com>
2022-12-11 15:02:01 +0000

    Versão simples da Parte 3 concluída!
    Ficou bem simples, mas se não der pra entregar de qualquer outra maneira essa já dá pro gasto. Como é bastante igual à daquele vídeo, vale a pena modificar um pouco o código ainda.

--- ProjetoVisaoComputacional/TestesFase3.py
+++ ProjetoVisaoComputacional/TestesFase3.py
@@ -4,26 +4,23 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5) #retorna a posição de todos os rostos
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) # retorna a posição de todos os rostos
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
-        roi_gray = gray[y:y+w, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
 
-
-
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame[:, ::-1, :])
     if cv2.waitKey(1) == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+# 2 problemas:
+# 1 - onde deixar o face cascade? ✔
+# 2 - onde como pegar a posição do retângulo ✔
+#     - criar uma variavel e igualar ela ao x do retangulo durante a exec do for
--- ProjetoVisaoComputacional/breakout_cv.py
+++ ProjetoVisaoComputacional/breakout_cv.py
@@ -1,6 +1,5 @@
 import cv2 as cv2
 
-
 def cv_setup(game):
     cv_init(game)
     cv_update(game)
@@ -12,25 +11,44 @@ def cv_init(game):
         game.cap.open(-1)
     # rest of init
 
+    global RecX # posição x do retângulo
+    RecX = game.cap.get(3)//2
+
+    global face_cascade # haar cascade de face
+    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
 
 def cv_update(game):
     cap = game.cap
     if not cap.isOpened():
         cap.open(-1)
     ret, image = cap.read()
-    image = image[:, ::-1, :]
     cv_process(image)
     cv_output(image)
-    # game.paddle.move(-1)
+
+    if RecX < (cap.get(3)//2):
+        game.paddle.move(5)
+    elif RecX > (cap.get(3)//2):
+        game.paddle.move(-5)
+
     game.after(1, cv_update, game)
 
 
 def cv_process(image):
     # main image processing code
+
+    global RecX
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # retorna a posição de todos os rostos
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 5)
+        RecX = x
+
     pass
 
 
 def cv_output(image):
-    cv2.imshow("Image", image)
+    cv2.imshow("Image", image[:, ::-1, :])
     # rest of output rendering
     cv2.waitKey(1)
